8bacc56ff86e0db7f6d11f204a6e0225cc4db864
Mohtasim <mohtasim317@hotmail.com>
2024-09-09 12:33:14 -0400

    Wrote function to generate categories

--- src/QuestionList.js
+++ src/QuestionList.js
@@ -16,15 +16,37 @@ export default function QuestionList() {
   //   setCategories(jsonData);
   // };
 
+  const createCategories = (questionData) => {
+    let allCategories = {};
+    for (let question of questionData) {
+      let currentCategory = question.category;
+      if (!allCategories[currentCategory]) {
+        allCategories[currentCategory] = true;
+      }
+    }
+    setCategories(allCategories);
+  };
+
   useEffect(() => {
     setQuestionListData(questionData);
+    createCategories(questionData);
+    console.log(categories);
+    console.log(questionListData);
     console.log(submissionData);
   }, []);
 
   return (
     <>
-      {questionListData.map(({ id, name, category }) => {
-        if (categories) return <div>{name}</div>;
+      {/* {questionListData.map(({ id, name, category }) => {
+        return (
+          <div className="question">
+            <div>{name}</div>
+            <div>{category}</div>
+          </div>
+        );
+      })} */}
+      {Object.keys(categories).map((category) => {
+        return <div>{category}</div>;
       })}
     </>
   );
