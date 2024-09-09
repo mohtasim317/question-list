729ff1c2d4f6498c778d8ab7db6aa7accdedfac2
Mohtasim <mohtasim317@hotmail.com>
2024-09-09 11:22:29 -0400

    Remoed unused files

---
+++ src/QuestionList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const QUESTIONS_API_BASE_URL = 'https://api.frontendexpert.io/api/fe/questions';
+const SUBMISSIONS_API_BASE_URL = 'https://api.frontendexpert.io/api/fe/submissions';
+
+export default function QuestionList() {
+  // Write your code here.
+
+  return (
+    <>
+      {/* Write your code here. */}
+    </>
+  );
+}
